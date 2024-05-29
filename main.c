edc2934b3eae4fe14b2ef0132fc6f2a8f83d1bf6
JaleMova <78921128+JaleMova@users.noreply.github.com>
2024-05-29 11:45:16 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-int main()
-{
+#include <stdio.h> 
 
-  for (int i = 0; i < 32; i++)
-  {
-    printf("Hello School\n");
-  }
-  return 0;
+int main()
+{    for (int i = 0; i < 32; i++)
+    {
+        printf("Hello School\n");
+    }
+    return 0; 
 }
